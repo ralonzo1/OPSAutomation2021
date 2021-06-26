41c542efe43c5b526350313b68b6ff73e864b15d
Philip De Lorenzo <philip@Philips-MBP.router.taotronics.com>
2021-06-26 11:45:33 -0700

    Add latest iteration of the code base

--- Makefile
+++ Makefile
@@ -1,9 +1,52 @@
 shell = ${SHELL}
 .PHONY: all clean test default hook unhook
+project := OPSAutomation
 
-setup:
-	$(info ******** Setup Terraform ********)
-	@cp /.config/.env .
-	# We want to copy the versioned source file to the root and use this, it can be altered for use but is ignored in git
-	@/bin/bash -c ". .env"
-	@chmod +x scripts/setup.sh && ./scripts/setup.sh
+default:
+	echo 'Makefile default target!'
+
+dev-tools:
+	$(info ******** Installing ASDF ********)
+	@cd scripts || exit 1 && chmod +x asdf_install.sh && ./asdf_install.sh
+	$(info ******** Installing Concourse Fly CLI ********)
+	@cd scripts || exit 1 && chmod +x fly_install.sh && ./fly_install.sh
+
+hook:
+	# TODO: DESIGN THE GITHOOK
+	#git config core.hooksPath .githooks
+unhook:
+	git config --unset core.hooksPath
+
+install_asdf:
+	cd scripts || exit 1 && chmod +x asdf_install.sh && ./asdf_install.sh
+
+dev_docker:
+	$(info ******** Installing Docker Environment ********)
+	@cd build_scripts/dev || exit 1 && ./build_dev_docker.sh
+
+dev_local:
+	$(info ******** Installing Local Environment ********)
+	@make install
+	@make super_user
+	@make start_server
+
+start_server:
+	$(info ******** Installing Python Environment and Starting Django Services ********)
+	@dev/local/.python/bin/python ${project}/manage.py runserver
+
+install:
+	$(info ******** Installing Python Environment ********)
+	@ ./scripts/pre-configure.sh
+	@./scripts/setup.sh -c
+	@./scripts/setup.sh -i
+
+super_user:
+	$(info ******** Create the SuperUser Account ********)
+	@./scripts/setup.sh -s
+
+clean:
+	$(info ******** Removing Python Environments ********)
+	@./scripts/setup.sh -c
+
+	$(info ******** Removing Local Database Backups ********)
+	@if [ -d database_backups ]; then rm -rf database_backups; fi
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,28 +1,147 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Author Philip De Lorenzo
+# Copyright (C) 2021
+# All rights reserved
+#
 
 set -euo pipefail
+project="OPSAutomation"
+projdir="OPSAutomation"
 
-create_creds () {
-    cat <<CREDS
-    [ops]
-    aws_access_key_id = ""
-    aws_secret_access_key = ""
+# Setting the relative base directory
+BASEDIR=$(realpath "$(dirname "$0")")
+BASE="${BASEDIR}/.."
+DEV="${BASE}/dev/local"
+PYDIR=".python"
+APPDIR="${BASE}/${project}/${projdir}"
 
-CREDS
+if [[ -f "${BASE}/${project}/.app_list" ]]; then
+    APPS_FILE="${BASE}/${project}/.app_list"
+elif [[ -f "${APPDIR}/.app_list" ]]; then
+    APPS_FILE="${APPDIR}/.app_list"
+fi
+
+# Creates the superuser for Django
+superuser ()
+{
+    source ${BASE}/.env
+    cd ${BASE}/${project} || exit 1 && ${DEV}/${PYDIR}/bin/python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_USER}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')"
 }
 
-terraform () {
-    # Setup the terraform settings if not exists
-    if [ ! ~/.aws ]; then
-        mkdir ~/.aws
-        touch ~/.aws/credentials
-        create_creds
+# Runs the installation of Python and packages
+install ()
+{
+    # Setting config
+    if [ -f ${BASE}/.env ]; then
+        : # Do nothing here if the conditional matches
     else
-        if [ -f ~/.aws/credentials ]; then
-            echo "There is already a credentials file. Fill out the needed data fields provided in the README."
+        if [ -z "${GITHUB_ACTION+x}" ]; then
+            cd ${BASE}/config || exit 1 && cp .env ..
+            echo "Please add environment configs to the .env file."
+            exit 1 # Creates the .env if it is not already there.
+        else
+            cd ${BASE}/config || exit 1 && cp .env .. # Creates the .env if it is not already there.
         fi
     fi
+
+    if [[ -d "${DEV}/${PYDIR}" ]]; then
+        rm -rf ${DEV}/${PYDIR}
+    fi
+
+    # Runs the setup and installs the application for use
+    python3 -m virtualenv ${DEV}/${PYDIR}
+
+    # Pip packagesss
+    cd ${APPDIR} || exit 1 && ${DEV}/${PYDIR}/bin/pip install --upgrade pip && ${DEV}/${PYDIR}/bin/pip install -r ../requirements.txt
+
+    # Run the migrations
+    while IFS= read -r line
+    do
+        cd ${BASE}/${project} || exit 1 && ${DEV}/${PYDIR}/bin/python manage.py makemigrations $line
+    done < $APPS_FILE
+
+    cd ${BASE}/${project} || exit 1 && ${DEV}/${PYDIR}/bin/python manage.py migrate # Commit migrations
+
+    # Create the Super User from .env
+    source ${BASE}/.env
+
+    echo "Create the super user for the application..."
+    echo "Change directories to the \'${projdir}\' directory, and \'$> run ${DEV}/${PYDIR}/bin/python manage.py createsuperuser\'"
+    echo ""
+    echo ""
+    echo "Exporting DJANGO_SETTINGS_MODULE"
+    export DJANGO_SETTINGS_MODULE=${projdir}.settings
 }
 
-terraform
-export AWS_PROFILE=ops
+clean ()
+{
+    echo "Cleaning the project of cached files and migrations."
+    if [[ -f "${APPDIR}/db.sqlite3" ]]; then rm -rf ${APPDIR}/db.*; fi
+    if [[ -d "${APPDIR}/__pycache__" ]]; then rm -rf ${APPDIR}/__pycache__; fi
+    if [[ -d "${APPDIR}/${projdir}/__pycache__" ]]; then rm -rf ${APPDIR}/${projdir}/__pycache__; fi
+
+    # Cleaning apps
+    while IFS= read -r line
+    do
+        echo "Removing ${APPDIR}/${line}/migrations"
+        if [[ -d "${APPDIR}/${line}/migrations" ]]; then rm -rf ${APPDIR}/${line}/migrations; fi
+
+        echo "Removing ${APPDIR}/${line}/__pycache__"
+        if [[ -d "${APPDIR}/${line}/__pycache__" ]]; then rm -rf ${APPDIR}/${line}/__pycache__; fi
+
+        echo "Removing ${APPDIR}/db.sqlite3"
+        if [[ -f "${APPDIR}/db.sqlite3" ]]; then
+            rm -rf ${APPDIR}/db.sqlite3
+        fi
+
+        echo "Removing ${APPDIR}/${line}/db.sqlite3"
+        if [[ -f "${APPDIR}/${line}/db.sqlite3" ]]; then
+            rm -rf ${APPDIR}/${line}/db.sqlite3
+        fi
+
+        echo "Removing ${BASE}/${project}/db.sqlite3"
+        if [[ -f "${BASE}/${project}/db.sqlite3" ]]; then
+            rm -rf ${BASE}/${project}/db.sqlite3
+        fi
+
+        echo "Removing ${BASE}/${project}/${line}/db.sqlite3"
+        if [[ -f "${BASE}/${project}/${line}/db.sqlite3" ]]; then
+            rm -rf ${BASE}/${project}/${line}/db.sqlite3
+        fi
+
+    done < $APPS_FILE
+
+    echo "Removing Python Virtual Environment"
+
+    if [[ -d "${DEV}/${PYDIR}" ]]; then echo "Removing local development Python installation..."; rm -rf ${DEV}/${PYDIR}; fi # Removes the local .python installation folder
+
+    # Remove the .superuser file
+    if [ -f ${BASE}/${project}/.superuser ]; then rm ${BASE}/${project}/.superuser; fi
+}
+
+database_init ()
+{
+    ${DEV}/${PYDIR}/bin/python ${BASEDIR}/${project}/.groups_init.py
+}
+
+usage() { echo "Usage: $0 -c (clean) -i (install) -d (database_init) -s (create_superuser)"; exit 0; }
+[ $# -eq 0 ] && usage
+while getopts ":icds" o; do
+    case "${o}" in
+        c)
+            clean
+            ;;
+        i)
+            install
+            ;;
+        d)
+            database_init
+            ;;
+        s)
+            superuser
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
